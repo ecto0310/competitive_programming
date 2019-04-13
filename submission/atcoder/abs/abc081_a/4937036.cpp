3eb83f9f49994c5077189cbf1887ff4068ef8e24
ecto0310 <ecto0310@gmail.com>
2019-04-13 19:44:23 +0900

    Add submission/atcoder/abs/abc081_a/4937036.cpp

---
+++ submission/atcoder/abs/abc081_a/4937036.cpp
@@ -0,0 +1,20 @@
+// URL: https://atcoder.jp/contests/abs/submissions/4937036
+// Date: Sat, 13 Apr 2019 19:44:23 +0900
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  string s;
+  cin>> s;
+
+  int ret=0;
+  if(s[0]=='1')ret++;
+  if(s[1]=='1')ret++;
+  if(s[2]=='1')ret++;
+  cout<< ret<< endl;
+
+  return 0;
+}
