e8a670c9068b4ba46d368cb03bbaf95b419305b8
ecto0310 <ecto0310@gmail.com>
2019-04-13 19:43:02 +0900

    Add submission/atcoder/abs/practice_1/4937021.cpp

---
+++ submission/atcoder/abs/practice_1/4937021.cpp
@@ -0,0 +1,19 @@
+// URL: https://atcoder.jp/contests/abs/submissions/4937021
+// Date: Sat, 13 Apr 2019 19:43:02 +0900
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int a,b,c;
+  string s;
+  cin>> a;
+  cin>> b>> c;
+  cin>> s;
+
+  cout<< a+b+c<< " "<< s<< endl;
+
+  return 0;
+}
