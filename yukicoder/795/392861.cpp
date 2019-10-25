6f15de651d4fec693a22471d6f849b45497638a5
ecto0310 <ecto0310@gmail.com>
2019-10-25 20:03:23 +0900

    Add yukicoder/795/392861.cpp

---
+++ yukicoder/795/392861.cpp
@@ -0,0 +1,15 @@
+// URL: https://yukicoder.me/submissions/392861
+// Date: Fri, 25 Oct 2019 20:03:23 +0900
+// Language: C++14
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 n, m;
+  cin >> n >> m;
+  cout << "Yes" << endl;
+  return 0;
+}
