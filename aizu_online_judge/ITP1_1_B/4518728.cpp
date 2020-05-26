dfe89a610a253fcce946b31b50a9ca3c0da1d1cd
ecto0310 <ecto0310@gmail.com>
2020-05-27 08:50:01 +0900

    Add aizu_online_judge/ITP1_1_B/4518728.cpp

---
+++ aizu_online_judge/ITP1_1_B/4518728.cpp
@@ -0,0 +1,15 @@
+// URL: http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=4518728
+// Date: Wed, 27 May 2020 08:50:01 +0900
+// Language: C++14
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 x;
+  cin >> x;
+  cout << x * x * x << endl;
+  return 0;
+}
