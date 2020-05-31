f1349e7acdab68a91bbf569a879d5a8e55fdd2cb
ecto0310 <ecto0310@gmail.com>
2020-05-31 21:00:21 +0900

    Add atcoder/abc169/abc169_a/13774318.cpp

---
+++ atcoder/abc169/abc169_a/13774318.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/abc169/submissions/13774318
+// Date: Sun, 31 May 2020 21:00:21 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 A, B;
+  cin >> A >> B;
+  cout << A * B << endl;
+  return 0;
+}
