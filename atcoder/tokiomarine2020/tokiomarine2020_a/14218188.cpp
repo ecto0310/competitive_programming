b37fb1335eb328289a0bfc31ecf752171459b500
ecto0310 <ecto0310@gmail.com>
2020-06-13 21:00:38 +0900

    Add atcoder/tokiomarine2020/tokiomarine2020_a/14218188.cpp

---
+++ atcoder/tokiomarine2020/tokiomarine2020_a/14218188.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/tokiomarine2020/submissions/14218188
+// Date: Sat, 13 Jun 2020 21:00:38 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  string S;
+  cin >> S;
+  cout << S.substr(0, 3) << endl;
+  return 0;
+}
