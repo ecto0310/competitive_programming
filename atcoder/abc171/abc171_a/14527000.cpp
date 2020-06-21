d0a328cf241d58fd260a40de9c6d876041d053ec
ecto0310 <ecto0310@gmail.com>
2020-06-21 21:01:03 +0900

    Add atcoder/abc171/abc171_a/14527000.cpp

---
+++ atcoder/abc171/abc171_a/14527000.cpp
@@ -0,0 +1,18 @@
+// URL: https://atcoder.jp/contests/abc171/submissions/14527000
+// Date: Sun, 21 Jun 2020 21:01:03 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  string a;
+  cin >> a;
+  if ('a' <= a[0] && a[0] <= 'z')
+    cout << "a" << endl;
+  else
+    cout << "A" << endl;
+  return 0;
+}
