dd4d5f2910409a2e587d71044108002901d11cda
ecto0310 <ecto0310@gmail.com>
2020-06-27 21:00:20 +0900

    Add atcoder/abc172/abc172_a/14719672.cpp

---
+++ atcoder/abc172/abc172_a/14719672.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/abc172/submissions/14719672
+// Date: Sat, 27 Jun 2020 21:00:20 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 a;
+  cin >> a;
+  cout << a + a * a + a * a * a << endl;
+  return 0;
+}
