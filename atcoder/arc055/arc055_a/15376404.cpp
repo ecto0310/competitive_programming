6f2ffe4d837a0f69a6bb306c00cb1871549fe11c
ecto0310 <ecto0310@gmail.com>
2020-07-23 20:17:11 +0900

    Add atcoder/arc055/arc055_a/15376404.cpp

---
+++ atcoder/arc055/arc055_a/15376404.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/arc055/submissions/15376404
+// Date: Thu, 23 Jul 2020 20:17:11 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N;
+  cin >> N;
+  cout << "1" << string(N - 1,'0') << "7" << endl;
+  return 0;
+}
