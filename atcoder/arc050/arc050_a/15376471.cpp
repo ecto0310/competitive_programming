4c8cebe09d9b31fed2e94a5e9295b378cf035104
ecto0310 <ecto0310@gmail.com>
2020-07-23 20:21:37 +0900

    Add atcoder/arc050/arc050_a/15376471.cpp

---
+++ atcoder/arc050/arc050_a/15376471.cpp
@@ -0,0 +1,18 @@
+// URL: https://atcoder.jp/contests/arc050/submissions/15376471
+// Date: Thu, 23 Jul 2020 20:21:37 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  char C, c;
+  cin >> C >> c;
+  if (c + ('A' - 'a') == C)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
