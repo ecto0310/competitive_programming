88b91d935380bb88b1db5d74a16628eadd6ceb5f
ecto0310 <ecto0310@gmail.com>
2020-08-02 21:01:48 +0900

    Add atcoder/abc174/abc174_b/15593371.cpp

---
+++ atcoder/abc174/abc174_b/15593371.cpp
@@ -0,0 +1,23 @@
+// URL: https://atcoder.jp/contests/abc174/submissions/15593371
+// Date: Sun, 02 Aug 2020 21:01:48 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N, D;
+  cin >> N >> D;
+  i64 ans = 0;
+  for (i64 i = 0; i < N; i++)
+  {
+    i64 X, Y;
+    cin >> X >> Y;
+    if (X * X + Y * Y <= D * D)
+      ans++;
+  }
+  cout << ans << endl;
+  return 0;
+}
