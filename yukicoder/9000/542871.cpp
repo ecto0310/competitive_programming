254cb4b42310b17c1e992c5797a9d1a5731a7da7
ecto0310 <ecto0310@gmail.com>
2020-08-29 07:00:09 +0900

    Add yukicoder/9000/542871.cpp

---
+++ yukicoder/9000/542871.cpp
@@ -0,0 +1,13 @@
+// URL: https://yukicoder.me/submissions/542871
+// Date: Sat, 29 Aug 2020 07:00:09 +0900
+// Language: C++17(1z)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  cout << "Hello World!" << endl;
+  return 0;
+}
