e24b96ff33111061898e645deef4c4c26d36e9ab
ecto0310 <ecto0310@gmail.com>
2020-08-29 07:00:51 +0900

    Add yukicoder/480/542872.cpp

---
+++ yukicoder/480/542872.cpp
@@ -0,0 +1,15 @@
+// URL: https://yukicoder.me/submissions/542872
+// Date: Sat, 29 Aug 2020 07:00:51 +0900
+// Language: C++17(1z)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N;
+  cin >> N;
+  cout << N * (N + 1) / 2 << endl;
+  return 0;
+}
