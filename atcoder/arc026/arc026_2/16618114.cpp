11375c8fe93ad9fc3d3b8bdc68474f960550dc2c
ecto0310 <ecto0310@gmail.com>
2020-09-10 23:47:19 +0900

    Add atcoder/arc026/arc026_2/16618114.cpp

---
+++ atcoder/arc026/arc026_2/16618114.cpp
@@ -0,0 +1,28 @@
+// URL: https://atcoder.jp/contests/arc026/submissions/16618114
+// Date: Thu, 10 Sep 2020 23:47:19 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N;
+  cin >> N;
+  i64 sum = -N;
+  for (i64 i = 1; i * i <= N; i++)
+    if (N % i == 0)
+    {
+      sum += i;
+      if (N / i != i)
+        sum += N / i;
+    }
+  if (N == sum)
+    cout << "Perfect" << endl;
+  else if (N < sum)
+    cout << "Abundant" << endl;
+  else
+    cout << "Deficient" << endl;
+  return 0;
+}
