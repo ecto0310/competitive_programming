ead26e9cce4406a0eabbdf577fea91c99e2669dd
ecto0310 <ecto0310@gmail.com>
2020-11-05 23:41:47 +0900

    Add atcoder/abc180/abc180_a/17894817.cpp

---
+++ atcoder/abc180/abc180_a/17894817.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/abc180/submissions/17894817
+// Date: Thu, 05 Nov 2020 23:41:47 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N, A, B;
+  cin >> N >> A >> B;
+  cout << N - A + B << endl;
+  return 0;
+}
