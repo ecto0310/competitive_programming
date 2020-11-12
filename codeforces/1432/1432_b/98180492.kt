4cd4818ecd674f0bb7e238734f21092702eb5273
ecto0310 <ecto0310@gmail.com>
2020-11-12 17:02:32 +0900

    Add codeforces/1432/1432_b/98180492.kt

---
+++ codeforces/1432/1432_b/98180492.kt
@@ -0,0 +1,10 @@
+// URL: https://codeforces.com/contest/1432/submission/98180492
+// Date: Thu, 12 Nov 2020 17:02:32 +0900
+// Language: Kotlin
+fun main() {
+  val t = readLine()!!.toInt()
+  for (i in 0..t - 1) {
+  val n = readLine()!!.toInt()
+  println((n - 1) / 2)
+}
+}
