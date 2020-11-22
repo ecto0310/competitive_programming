f0ddb88d5e2e7ce1bca9160931a12122951bc7e5
ecto0310 <ecto0310@gmail.com>
2020-11-23 01:41:26 +0900

    Add submission/atcoder/abc181/abc181_a/18353983.cpp

---
+++ submission/atcoder/abc181/abc181_a/18353983.cpp
@@ -0,0 +1,18 @@
+// URL: https://atcoder.jp/contests/abc181/submissions/18353983
+// Date: Mon, 23 Nov 2020 01:41:26 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N;
+  cin >> N;
+  if (N % 2 == 0)
+    cout << "White" << endl;
+  else
+    cout << "Black" << endl;
+  return 0;
+}
