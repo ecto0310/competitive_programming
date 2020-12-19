f7189e416f962c23770a4207318d09c4c64624a0
ecto0310 <ecto0310@gmail.com>
2020-12-19 21:00:24 +0900

    Add submission/atcoder/abc186/abc186_a/18858928.cpp

---
+++ submission/atcoder/abc186/abc186_a/18858928.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/abc186/submissions/18858928
+// Date: Sat, 19 Dec 2020 21:00:24 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 N, W;
+  cin >> N >> W;
+  cout << N / W << endl;
+  return 0;
+}
