b87e30a3e72c5ab914a7faa00b6873fdbf58c1a3
ecto0310 <ecto0310@gmail.com>
2021-01-26 18:39:43 +0900

    Add submission/atcoder/abc184/abc184_a/19705690.cpp

---
+++ submission/atcoder/abc184/abc184_a/19705690.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc184/submissions/19705690
+// Date: Tue, 26 Jan 2021 18:39:43 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+#include <unistd.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 a, b, c, d;
+  cin >> a >> b >> c >> d;
+  cout << a * d - b * c << endl;
+  return 0;
+}
