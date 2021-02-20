752eec5191d53fd6cf7fa8d5073db83c557089ba
ecto0310 <ecto0310@gmail.com>
2021-02-20 21:00:30 +0900

    Add submission/atcoder/abc192/abc192_a/20281804.cpp

---
+++ submission/atcoder/abc192/abc192_a/20281804.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/abc192/submissions/20281804
+// Date: Sat, 20 Feb 2021 21:00:30 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main() {
+    i64 X;
+    cin >> X;
+    cout << 100 - X % 100 << endl;
+    return 0;
+}
