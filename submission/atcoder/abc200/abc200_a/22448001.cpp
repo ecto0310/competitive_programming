cb45eeea973ffdfad1a51d306d8837d38a6857cc
ecto0310 <ecto0310@gmail.com>
2021-05-09 02:53:37 +0900

    Add submission/atcoder/abc200/abc200_a/22448001.cpp

---
+++ submission/atcoder/abc200/abc200_a/22448001.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/abc200/submissions/22448001
+// Date: Sun, 09 May 2021 02:53:37 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main() {
+    i64 N;
+    cin >> N;
+    cout << (N - 1) / 100 + 1 << endl;
+    return 0;
+}
