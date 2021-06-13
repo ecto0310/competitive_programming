cbec2bd41c274eb2f5605f6ab0608a22321a74cc
ecto0310 <ecto0310@gmail.com>
2021-06-13 21:01:12 +0900

    Add submission/atcoder/abc205/abc205_a/23397543.cpp

---
+++ submission/atcoder/abc205/abc205_a/23397543.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/abc205/submissions/23397543
+// Date: Sun, 13 Jun 2021 21:01:12 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main() {
+    i64 A, B;
+    cin >> A >> B;
+    cout << (double) A / 100 * B << endl;
+    return 0;
+}
